a8dc4e6f10cf4a1108155c64bfd5c577124c4c2b
David Harty <dwh@msi-ge66.whale>
2023-07-24 15:02:56 -0700

    Modifying build-linux.sh to create the lib directory needed by make-archive.sh. build-linux builds a shared libary. That library is now copied into the .lib folder for make-archive.sh

--- build-linux.sh
+++ build-linux.sh
@@ -8,3 +8,9 @@
 --include_ops_by_config model.required_operators_and_types.config \
 --enable_reduced_operator_type_support \
 --skip_tests
+
+# make-archive.sh expect the build process to but files in a lib directory s
+# that is included in the archive.
+mkdir -p "lib"
+# using find to avoid cp stat errors with wildcards
+find ./onnxruntime/build/Linux_x86_64/MinSizeRel -name "libonnxruntime.so*" -exec cp {} lib/ \;

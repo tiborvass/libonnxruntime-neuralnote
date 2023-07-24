14adcc7810cbe4b224e09986892ac830ecb48ab2
David Harty <dwh@msi-ge66.whale>
2023-07-24 15:47:11 -0700

    Cleaning up comments.

--- build-linux.sh
+++ build-linux.sh
@@ -13,8 +13,8 @@ onnx_config="${1:-model.required_operators_and_types.config}"
 --enable_reduced_operator_type_support \
 --skip_tests
 
-# make-archive.sh expect the build process to but files in a lib directory s
-# that is included in the archive.
+# make-archive.sh expects the build process to put files in the ./lib directory
+# which is included in the archive.
 mkdir -p "lib"
 # using find to avoid cp stat errors with wildcards
 find ./onnxruntime/build/Linux_x86_64/MinSizeRel -name "libonnxruntime.so*" -exec cp {} lib/ \;

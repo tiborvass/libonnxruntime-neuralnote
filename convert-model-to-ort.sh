1ac0228d5d07890c0a504fbdeb6588e00afe1b8a
polygon <polygon@wh2.tu-dresden.de>
2023-11-02 17:59:45 +0100

    Adaptations for Linux build

--- convert-model-to-ort.sh
+++ convert-model-to-ort.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #python -m tf2onnx.convert --saved-model model --output model.onnx --opset 13
 python -m onnxruntime.tools.convert_onnx_models_to_ort $1 --enable_type_reduction
 rm -rf ./model/
 mkdir -p ./model
 # python verify_model.py
-python -m bin2c -o ./model/model.ort model.ort
+python -m bin2c -o ./model/model.ort model.ort || bin2c > ./model/model.ort < model.ort
